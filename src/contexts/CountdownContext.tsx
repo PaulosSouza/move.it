ac6512a136eae4cbe03fb4711ce278bb04943c7e
Paulo Henrique <phferreirasouza106@gmail.com>
2021-02-27 17:58:11 -0300

    fix: change countdown

--- src/contexts/CountdownContext.tsx
+++ src/contexts/CountdownContext.tsx
@@ -21,7 +21,7 @@ let countdownTimeout: NodeJS.Timeout;
 export function CountownProvider({ children }: CountdownProviderProps) {
   const { startNewChallenge } = useContext(ChallengesContext);
 
-  const [time, setTime] = useState(0.1 * 60);
+  const [time, setTime] = useState(25 * 60);
   const [isActive, setIsActive] = useState(false);
   const [hasFinished, setHasFinished] = useState(false);
 
@@ -35,7 +35,7 @@ export function CountownProvider({ children }: CountdownProviderProps) {
   function resetCountdown() {
     clearInterval(countdownTimeout);
     setIsActive(false);
-    setTime(0.1 * 60);
+    setTime(25 * 60);
     setHasFinished(false);
   }
 
